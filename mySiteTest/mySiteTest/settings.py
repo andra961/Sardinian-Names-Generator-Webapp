9015e93d1bd4d5a46d329a78845f553f57b77cb5
andrea corronca <andrac1996@gmail.com>
2020-12-12 17:18:44 +0100

    nice commit

--- mySiteTest/mySiteTest/settings.py
+++ mySiteTest/mySiteTest/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'nameGenerator.apps.NamegeneratorConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
